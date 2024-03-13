488c3d0056f91e0988e2284fa5d52581f0280f1e
resellernewby <resellernewby@gmail.com>
2024-03-14 06:34:49 +0700

    penambahan model-migrasi-table profile klinik

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\ProfileClinic;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 
@@ -20,6 +22,15 @@ class DatabaseSeeder extends Seeder
             'email' => 'admin@gmail.com',
             'password' => Hash::make('12345678'),
             'role' => 'admin',
+        ]);
+
+        ProfileClinic::create([
+            'name' => 'Masbukin Clinic',
+            'address' => 'Jl. Palka 15, Bantarwaru Cinangka Serang',
+            'phone' => '0877387388377',
+            'email' => 'dr.johndoe@gmail.com',
+            'doctor_name' => 'Dr. John doe',
+            'unique_code' => 'NHD293790',
         ]);
     }
 }
