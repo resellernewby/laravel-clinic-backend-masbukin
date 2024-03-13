d5eb15cd78a782850424e3940c849da202fa5ac9
resellernewby <resellernewby@gmail.com>
2024-03-13 20:45:37 +0700

    add view dashboard, login, register, add fortify

---
+++ resources/views/components/auth-footer.blade.php
@@ -0,0 +1,3 @@
+  <div class="simple-footer">
+      Copyright &copy; Masbukin 2024
+  </div>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.auth.login');
+});
+
+
+Route::middleware(['auth'])->group(function(){
+    Route::get('/home', function(){
+        return view('dashboard');
+    })->name('dashboard');
 });
